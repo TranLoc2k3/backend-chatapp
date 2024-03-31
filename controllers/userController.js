63010f9c7d0f3e1e7593d7a8a009da0ddee25527
TranLoc2k3 <tranloc120603@gmail.com>
2024-03-31 13:48:05 +0700

    update code

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,21 @@ const getAllUser = async (req, res) => {
   }
 };
 
+const getUserByID = async (req, res) => {
+  try {
+    const userID = req.body.id;
+    const myUser = await UserModel.get(userID);
+    if (myUser) {
+      return res.status(200).json(myUser);
+    }
+    return res.status(404).json({ message: "User not found" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   getAllUser,
+  getUserByID
 };
